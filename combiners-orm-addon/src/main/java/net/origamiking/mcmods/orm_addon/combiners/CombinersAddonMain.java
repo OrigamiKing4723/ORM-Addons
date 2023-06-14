bcde67095ddfb4833604cfcaed0d8528a633908f
OrigamiKing3612 <origamiking3612@gmail.com>
2023-06-13 19:58:36 -0500

    Updated Combiners Addon

---
+++ combiners-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/combiners/CombinersAddonMain.java
@@ -0,0 +1,18 @@
+package net.origamiking.mcmods.orm_addon.combiners;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.origamiking.mcmods.orm.OrmMain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CombinersAddonMain implements ModInitializer {
+	public static final String MOD_ID = "combiners";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String VERSION = "0.1.0-1.20.1";
+
+	@Override
+	public void onInitialize() {
+		CombinersAddonMain.LOGGER.info("Starting "+ MOD_ID + "-" + VERSION + " for ORM-" + OrmMain.VERSION);
+	}
+}
--- combiners-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/combiners/datagen/CombinersAddonDataGen.java
+++ combiners-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/combiners/datagen/CombinersAddonDataGen.java
@@ -1,9 +1,9 @@
-package net.origamiking.mcmods.orm_addon.combiners;
+package net.origamiking.mcmods.orm_addon.combiners.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class CombinersAddonDataGen implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
