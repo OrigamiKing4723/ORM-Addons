e1848e6c4e8e21fe55520b156c25453642dfebbf
OrigamiKing3612 <origamiking3612@gmail.com>
2023-06-20 19:19:39 -0500

    Updated orm

--- movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/movie_characters/datagen/ModRecipeGenerator.java
+++ movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/movie_characters/datagen/ModRecipeGenerator.java
@@ -3,13 +3,11 @@ package net.origamiking.mcmods.movie_characters.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.recipe.book.RecipeCategory;
 import net.origamiking.mcmods.movie_characters.armor.movie_soundwave.MovieSoundwave;
 import net.origamiking.mcmods.movie_characters.items.chips.robo_1.MovieSoundwaveChip;
+import net.origamiking.mcmods.orm.datagen.recipes.ArmorRecipes;
 
 import java.util.function.Consumer;
-
-import static net.origamiking.mcmods.orm.datagen.ModRecipeGenerator.*;
 
 public class ModRecipeGenerator extends FabricRecipeProvider {
     public ModRecipeGenerator(FabricDataOutput output) {
@@ -18,14 +16,6 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
-        offerTransformerHelmet(exporter, MovieSoundwave.HELMET, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
-        offerTransformerChestplate(exporter, MovieSoundwave.CHESTPLATE, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
-        offerTransformerLeggings(exporter, MovieSoundwave.LEGGINGS, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
-        offerTransformerBoots(exporter, MovieSoundwave.BOOTS, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
-        offerTransformerCar(exporter, MovieSoundwave.CAR, MovieSoundwave.HELMET, MovieSoundwave.CHESTPLATE, MovieSoundwave.LEGGINGS, MovieSoundwave.BOOTS);
-
-
-
-        offerChipRefiningRecipe(exporter, RecipeCategory.MISC, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
+        ArmorRecipes.transformerRecipes(exporter, MovieSoundwave.HELMET, MovieSoundwave.CHESTPLATE, MovieSoundwave.LEGGINGS, MovieSoundwave.BOOTS, MovieSoundwave.CAR, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
     }
 }
