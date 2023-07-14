9295940aa15211a0b75737205c19f0cb6b72a637
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-14 11:54:19 -0500

    Updated orm

--- movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/datagen/ModRecipeGenerator.java
+++ movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/datagen/ModRecipeGenerator.java
@@ -3,11 +3,14 @@ package net.origamiking.mcmods.orm_addon.movie_characters.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.origamiking.mcmods.orm.datagen.recipes.providers.OrmRecipeProvider;
 import net.origamiking.mcmods.orm_addon.movie_characters.armor.movie_soundwave.MovieSoundwave;
 import net.origamiking.mcmods.orm_addon.movie_characters.items.chips.robo_1.MovieSoundwaveChip;
 import net.origamiking.mcmods.orm.datagen.recipes.ArmorRecipes;
 
 import java.util.function.Consumer;
+
+import static net.origamiking.mcmods.orm.datagen.recipes.providers.OrmRecipeProvider.transformerRecipes;
 
 public class ModRecipeGenerator extends FabricRecipeProvider {
     public ModRecipeGenerator(FabricDataOutput output) {
@@ -16,6 +19,6 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
-        ArmorRecipes.transformerRecipes(exporter, MovieSoundwave.HELMET, MovieSoundwave.CHESTPLATE, MovieSoundwave.LEGGINGS, MovieSoundwave.BOOTS, MovieSoundwave.CAR, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
+        transformerRecipes(exporter, MovieSoundwave.HELMET, MovieSoundwave.CHESTPLATE, MovieSoundwave.LEGGINGS, MovieSoundwave.BOOTS, MovieSoundwave.CAR, MovieSoundwaveChip.MOVIE_SOUNDWAVE_CHIP);
     }
 }
