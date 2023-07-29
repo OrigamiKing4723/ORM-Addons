cae38f28ae652af6b327e804ab572c50d6177797
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-29 12:12:59 -0500

    Updated ORM

--- movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/armor/SetEffects.java
+++ movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/armor/SetEffects.java
@@ -10,13 +10,10 @@ import net.origamiking.mcmods.orm_addon.movie_characters.armor.movie_soundwave.M
 public class SetEffects {
     public static void setEffects() {
         ServerTickEvents.END_SERVER_TICK.register(server -> server.getPlayerManager().getPlayerList().forEach(player -> {
-            boolean hasCompleteSet = false;
-            if (player.getEquippedStack(EquipmentSlot.HEAD).getItem() instanceof TransformerArmorItem &&
+            boolean hasCompleteSet = player.getEquippedStack(EquipmentSlot.HEAD).getItem() instanceof TransformerArmorItem &&
                     player.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof TransformerArmorItem &&
                     player.getEquippedStack(EquipmentSlot.LEGS).getItem() instanceof TransformerArmorItem &&
-                    player.getEquippedStack(EquipmentSlot.FEET).getItem() instanceof TransformerArmorItem) {
-                hasCompleteSet = true;
-            }
+                    player.getEquippedStack(EquipmentSlot.FEET).getItem() instanceof TransformerArmorItem;
             if (hasCompleteSet || player.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof MovieSoundwaveCarArmorItem) {
                 player.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 2, 0, false, false));
             } else {
--- movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/armor/movie_soundwave/MovieSoundwave.java
+++ movie-characters-orm-addon/src/main/java/net/origamiking/mcmods/orm_addon/movie_characters/armor/movie_soundwave/MovieSoundwave.java
@@ -2,15 +2,18 @@ package net.origamiking.mcmods.orm_addon.movie_characters.armor.movie_soundwave;
 
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterials;
-import net.origamiking.mcmods.orm_addon.movie_characters.utils.armor.ArmorUtils;
+import net.origamiking.mcmods.oapi.armor.ArmorUtils;
 import net.origamiking.mcmods.oapi.items.OrigamiItemSettings;
+import net.origamiking.mcmods.orm_addon.movie_characters.MovieCharactersModMain;
+
+import static net.origamiking.mcmods.oapi.items.ItemsUtils.registerItem;
 
 public class MovieSoundwave extends ArmorUtils {
-    public static final MovieSoundwaveCarArmorItem CAR = registerItem("movie_soundwave_car", new MovieSoundwaveCarArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new OrigamiItemSettings()));
-    public static final MovieSoundwaveArmorItem HELMET = registerItem("movie_soundwave_armor_helmet", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new OrigamiItemSettings()));
-    public static final MovieSoundwaveArmorItem CHESTPLATE = registerItem("movie_soundwave_armor_chestplate", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new OrigamiItemSettings()));
-    public static final MovieSoundwaveArmorItem LEGGINGS = registerItem("movie_soundwave_armor_leggings", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new OrigamiItemSettings()));
-    public static final MovieSoundwaveArmorItem BOOTS = registerItem("movie_soundwave_armor_boots", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.BOOTS, new OrigamiItemSettings()));
+    public static final MovieSoundwaveCarArmorItem CAR = registerItem(MovieCharactersModMain.MOD_ID, "movie_soundwave_car", new MovieSoundwaveCarArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new OrigamiItemSettings()));
+    public static final MovieSoundwaveArmorItem HELMET = registerItem(MovieCharactersModMain.MOD_ID, "movie_soundwave_armor_helmet", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new OrigamiItemSettings()));
+    public static final MovieSoundwaveArmorItem CHESTPLATE = registerItem(MovieCharactersModMain.MOD_ID, "movie_soundwave_armor_chestplate", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new OrigamiItemSettings()));
+    public static final MovieSoundwaveArmorItem LEGGINGS = registerItem(MovieCharactersModMain.MOD_ID, "movie_soundwave_armor_leggings", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new OrigamiItemSettings()));
+    public static final MovieSoundwaveArmorItem BOOTS = registerItem(MovieCharactersModMain.MOD_ID, "movie_soundwave_armor_boots", new MovieSoundwaveArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.BOOTS, new OrigamiItemSettings()));
     public static void get() {
 
     }
